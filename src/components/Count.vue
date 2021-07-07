85f906ca0e7863bd8a6ea31fa08d4e947ca4b77d
Yunsong Zhang <zhangyunsong199834@gmail.com>
2021-07-07 17:30:39 +0800

    76

--- src/components/Count.vue
+++ src/components/Count.vue
@@ -1,45 +1,40 @@
 <template>
     <div>
         <h2>当前求和为：{{sum}}</h2>
+        <h2>当前求和为：{{computedNumber}}</h2>
         <select v-model.number="number">
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
         </select> &nbsp;
-        <button @click="increment">+</button> &nbsp;
-        <button @click="decrement">-</button> &nbsp;
-        <button @click="incrementOdd">奇数+</button> &nbsp;
-        <button @click="incrementAsync">异步+</button>
+        <button @click="increment(number)">+</button> &nbsp;
+        <button @click="decrement(number)">-</button> &nbsp;
+        <button @click="incrementOdd(number)">奇数+</button> &nbsp;
+        <button @click="incrementAsync(number)">异步+</button>
     </div>
 </template>
 
 <script>
+    import {mapState,mapGetters,mapMutations,mapActions} from 'vuex'
     export default {
         name:'Count',
         data() {
             return {
-                sum:0,
+                // sum:0,
                 number:1, // 用户选择的数字
             }
         },
         methods: {
-            increment(){
-                this.sum += this.number
-            },
-            decrement(){
-                this.sum -= this.number
-            },
-            incrementOdd(){
-                if(this.sum % 2){
-                    this.sum += this.number
-                }
-            },
-            incrementAsync(){
-                setTimeout(() => {
-                    this.sum += this.number
-                }, 1000);
-            },
+            ...mapMutations({
+                increment:'INCREMENT',
+                decrement:'DECREMENT',
+            }),
+            ...mapActions(["incrementOdd",'incrementAsync']),
         },
+        computed:{
+            ...mapState(["sum"]),
+            ...mapGetters(['computedNumber']),
+        }
     }
 </script>
 
