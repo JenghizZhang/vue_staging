2af3370e2e8fdff1d683935ae4b2e80fe9c10427
Yunsong Zhang <zhangyunsong199834@gmail.com>
2021-07-15 16:18:01 +0800

    final

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,20 @@
 <template>
     <div>
-        <Header />
-        <div class="row">
-            <div class="col-xs-2 col-xs-offset-2">
-                <div class="list-group">
-                    <!-- Vue使用router-link实现路径改变 -->
-                    <router-link replace class="list-group-item" active-class="active" to="/about">About</router-link>
-                    <router-link replace class="list-group-item" active-class="active" to="/home">Home</router-link>
-                </div>
-            </div>
-            <div class="col-xs-6">
-                <div class="panel">
-                    <div class="panel-body">
-                        <!-- <About />
-                        <Home /> -->
-                        <router-view></router-view>
-                        <!-- <router-view name="Home"></router-view>
-                        <router-view name="Home2"></router-view> -->
-                    </div>
-                </div>
-            </div>
-        </div>
+        <h2>123</h2>
+        <button>原生的按钮</button>
+        <input type="text">
+        <hr>
+        <el-button type="success" plain>成功按钮</el-button>
+
     </div>
 </template>
 
 <script>
-    import Header from './components/Header'
-    // import About from './pages/About'
-    // import Home from './pages/Home'
-
     export default {
-        name:'App',
-        components:{Header,},
-        // components:{Header,About,Home},
+        name:'App'
     }
 </script>
+
+<style scoped>
+    
+</style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-
-import router from './router'
-
-
+// import ElementUI from 'element-ui';
+// import 'element-ui/lib/theme-chalk/index.css';
+import { Button } from 'element-ui'
 Vue.config.productionTip = false //关闭生产提示
+
+// Vue.use(ElementUI);
+Vue.component(Button.name, Button);
 
 new Vue({
     el:'#app',
-    router,
     render:h => h(App),
 })
